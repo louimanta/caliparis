d58bb35447f359e41d4eb80e8b70a4696e0c9680
louimanta <louimanta@gmail.com>
2025-11-28 06:36:44 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,41 +1,20 @@
-// models/product.js (ou dans votre index.js)
-const Product = sequelize.define('Product', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  videoUrl: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  stock: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  },
-  category: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  quality: {
-    type: DataTypes.STRING,
-    allowNull: true
-  }
-}, {
-  tableName: 'Products'
-});
+// models/index.js
+const { 
+  sequelize,
+  Sequelize,
+  Product,
+  Customer,
+  Order,
+  OrderItem,
+  Cart 
+} = require('./index');
+
+module.exports = {
+  sequelize,
+  Sequelize,
+  Product,
+  Customer,
+  Order,
+  OrderItem,
+  Cart
+};
